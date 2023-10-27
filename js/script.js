679d84d0aa523af8488a56831b0aed7cb4587110
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-27 15:45:46 +0200

    added paragraph and image

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ createApp ({
   data (){
     return {
       message: "Hello Vue!",
+      paragraph: "Did you know Vue.js version names are often derived from manga and anime?",
+      image: "./img/vueVersions.png",
     }
   }
 }
